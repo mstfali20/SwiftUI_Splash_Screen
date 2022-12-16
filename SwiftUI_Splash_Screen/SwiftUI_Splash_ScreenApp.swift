22d800828745da393a73d57b1e307a72a60875fc
Mustafa Ali KILCI <mustafa_klc997@hotmail.com>
2022-12-16 19:50:36 +0300

    Initial Commit

---
+++ SwiftUI_Splash_Screen/SwiftUI_Splash_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Splash_ScreenApp.swift
+//  SwiftUI_Splash_Screen
+//
+//  Created by Mustafa Ali KILCI on 16.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Splash_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
